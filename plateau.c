7caf50a5bdda4334a505f26152e1675fdceeceae
Tom Payet <tompayet@openroam-prg-sg-1-173-107.net.univ-paris-diderot.fr>
2020-03-04 11:43:46 +0100

    plateau.c init robot

--- plateau.c
+++ plateau.c
@@ -1,14 +1,25 @@
 #include <ncurses.h>
+#include <robot.h>
 
-void plateau(){
+#define DELAY 33
+
+void plateau(robot r1, robot r2, robot r3, robot r4){
     WINDOW *boite;
     initscr();//Initialise la structure WINDOW et autres paramètres
-    boite = subwin(stdscr, 10, 10, LINES / 2, COLS /2);
-    box(boite, ACS_VLINE, ACS_HLINE); // ACS_VLINE et ACS_HLINE sont des constantes qui génèrent des bordures par défaut
-    refresh();
+    boite = subwin(stdscr, LINES, COLS,0,0);//On créer une fenetre de LINES lignes et de COL collonnes a partir de la pos (0,0)
+
+	//Robot
+	for (size_t i = 1; i < 100; i++)
+	{
+		wclear(boite);
+		box(boite, ACS_VLINE, ACS_HLINE); // ACS_VLINE et ACS_HLINE sont des constantes qui génèrent des bordures par défaut
+		mvwaddch(boite,1,i,'>');
+		wrefresh(boite);
+		napms(DELAY);
+	}
+
 
     getch();// On attend que l'utilisateur appui sur une touche pour quitter
     endwin();// Restaure les paramètres par défaut du terminal
-
     free(boite);
 }
