43dc2619d3202f074aa33a7470be50f806e8d0dd
berthel <antoine.berthel@etu.univ-paris-diderot.fr>
2020-04-15 19:44:40 +0200

    modif robot et missile

--- src/missile.c
+++ src/missile.c
@@ -1,20 +1,30 @@
 #include "missile.h"
+
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "main.h"
 #include "robot.h"
 
-void initMissile(missile *m, robot *parent) {
-    m->parent = parent;
-    m->position = parent->position;
+void initMissile(missile *m, robot *parent, int distance, int angle) {
+  m->parent = parent;
+  m->position = parent->position;
+  m->distance = 0;
+  m->distanceExplosion = distance;
+  m->angle = angle;
 }
 
 int explose(missile *m) {
-    m->parent->nb_missiles-=1;
-    free(m);
+  m->parent->nb_missiles -= 1;
+  free(m);
 }
 
-void miseAJourMissile(missile *m, pos position) {
-    m->position.x = position.x;
-    m->position.y = position.y;
+void miseAJourMissile(missile *m) {
+  m->distance += m->distanceExplosion - m->distance < 500
+                     ? m->distanceExplosion - m->distance
+                     : 500;
+  m->position.x += m->distance * (int)cos(m->angle);
+  m->position.y += m->distance * (int)sin(m->angle);
+  if (m->distance >= m->distanceExplosion) explose(m);
 }
