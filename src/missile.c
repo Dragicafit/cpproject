d9bec7150292cfdea2a5666cb69917f45821ed3e
payet <tom.payet1@gmail.com>
2020-05-09 21:50:49 +0200

    alignement missile robot

--- src/missile.c
+++ src/missile.c
@@ -24,17 +24,39 @@ missile *initMissile(robot *parent, int distance, int angle) {
 
 void explose(missile *m) { m->aExplose = 1; }
 
+char RinTrajectory(arene *a, missile *m) {
+  for (int i = 0; i < ROBOT_MAX; i++) {
+    if (a->l_robot[i] == m->parent ||
+        distance(a->l_robot[i]->position.x, a->l_robot[i]->position.y,
+                 m->parent->position.x, m->parent->position.y) >
+            distance(m->position.x, m->position.y, m->parent->position.x,
+                     m->parent->position.y)) {
+      continue;
+    }
+    int x1 = a->l_robot[i]->position.x - m->parent->position.x;
+    int y1 = a->l_robot[i]->position.y - m->parent->position.y;
+    int x2 = m->position.x - m->parent->position.x;
+    int y2 = m->position.y - m->parent->position.y;
+    if (x1 * y2 == y1 * x2) {
+      m->position = a->l_robot[i]->position;
+      exploseRobots(a, m);
+      return 1;
+    }
+  }
+  return 0;
+}
+
 void miseAJourMissile(arene *a, int i) {
   missile *m = a->l_missile[i];
-  int distance = m->distanceExplosion - m->distance < MISSILE_SPEED
-                     ? m->distanceExplosion
-                     : MISSILE_SPEED;
-  distance *= UNITE_COEFF;
-  m->distance += distance;
+  int dist = m->distanceExplosion - m->distance < MISSILE_SPEED
+                 ? m->distanceExplosion
+                 : MISSILE_SPEED;
+  dist *= UNITE_COEFF;
+  m->distance += dist;
   float x = m->position.x;
   float y = m->position.y;
-  m->position.x = TargetX(m->position.x, m->angle, distance);
-  m->position.y = TargetY(m->position.y, m->angle, distance);
+  m->position.x = TargetX(m->position.x, m->angle, dist);
+  m->position.y = TargetY(m->position.y, m->angle, dist);
   if (m->position.x >= X) {
     m->position.x = X;
     m->position.y = (X - x) * (m->position.y - y) / (m->position.x - x);
@@ -59,6 +81,7 @@ void miseAJourMissile(arene *a, int i) {
     exploseRobots(a, m);
     return;
   }
+  if (RinTrajectory(a, m)) return;
   if (m->distance >= m->distanceExplosion) {
     exploseRobots(a, m);
   }
